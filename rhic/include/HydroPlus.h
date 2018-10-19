9983edc02dbc3a7939fc5c6b101bff42e29da159
Lipei Du <lpdu318@gmail.com>
2018-10-18 22:28:52 -0400

    initialization and entropy function of slow modes

--- rhic/include/HydroPlus.h
+++ rhic/include/HydroPlus.h
@@ -11,37 +11,40 @@
 #define HydroPlus_h
 
 #include "../include/DynamicalVariables.h"
+#include "../include/LatticeParameters.h"
+#include "../include/InitialConditionParameters.h"
+#include "../include/HydroParameters.h"
 
 #define NUMBER_SLOW_MODES 3
-
-typedef struct
-{
-    PRECISION *qvec[NUMBER_SLOW_MODES];
-} SLOW_MODES_Q;
 
 typedef struct
 {
     PRECISION *phiQ[NUMBER_SLOW_MODES];
 } SLOW_MODES;
 
-extern SLOW_MODES_Q *Qvec;
-extern SLOW_MODES *phiQ;
+extern SLOW_MODES *PhiQ, *PhiQp, *PhiQS; // Slow modes: updated, previous, intermediate values
+extern SLOW_MODES *eqPhiQ, *eqPhiQp, *eqPhiQS;  // Slow modes at equilibrium: updated, previous, intermediate values
 
-//correlation length
-PRECISION xi(PRECISION e, PRECISION rhob);
+extern PRECISION *Qvec; // Q vectors of slow modes
 
-//fluctuations at equilibrium
+void allocateHostMemorySlowModes(int len);
+
+PRECISION Cp(PRECISION e, PRECISION rhob);
+PRECISION xi(PRECISION e, PRECISION rhob);//correlation length
 PRECISION f2(PRECISION x); // Universal function f2, Eq.(89)
+
 PRECISION equilibriumPhi0(PRECISION e, PRECISION rhob); // magnitude of the fluctuation at equilibirium, Eq.(90)
 PRECISION equilibriumPhiQ(PRECISION e, PRECISION rhob, PRECISION Q); // cf. Eq.(89)
 
-//relaxation of fluctuations
 PRECISION relaxationCoefficientPhiQ(PRECISION e, PRECISION rhob, PRECISION Q);
 
-//source terms of extra modes
-void setSlowModesSourceTerms(PRECISION * const __restrict__ PhiQRHS, const PRECISION * const __restrict__ equilibriumPhiQ, const PRECISION * const __restrict__ PhiQ, const SLOW_MODES_Q * const __restrict__ Qvec, PRECISION e, PRECISION rhob, PRECISION ut, PRECISION dkvk);
+void setInitialConditionSlowModes(void * latticeParams, void * hydroParams);
 
-void loadSlowModesSourceTerms(const SLOW_MODES * const __restrict__ equilibriumSlowModes, const SLOW_MODES * const __restrict__ SlowModes, PRECISION * const __restrict__ S, const SLOW_MODES_Q * const __restrict__ Qvec, const FLUID_VELOCITY * const __restrict__ u, const PRECISION * const __restrict__ evec, const PRECISION * const __restrict__ rhobvec, int s, int d_ncx, int d_ncy, int d_ncz, PRECISION d_dx, PRECISION d_dy, PRECISION d_dz);
+PRECISION entropySlowModes(const PRECISION * const __restrict__ equilibriumPhiQ, const PRECISION * const __restrict__ PhiQ, const PRECISION * const __restrict__ Q);
+
+void setSlowModesSourceTerms(PRECISION * const __restrict__ PhiQRHS, const PRECISION * const __restrict__ equilibriumPhiQ, const PRECISION * const __restrict__ PhiQ, const PRECISION * const __restrict__ Qvec, PRECISION e, PRECISION rhob, PRECISION ut, PRECISION dkvk);
+
+void loadSlowModesSourceTerms(const SLOW_MODES * const __restrict__ equilibriumSlowModes, const SLOW_MODES * const __restrict__ SlowModes, PRECISION * const __restrict__ S, const PRECISION * const __restrict__ Qvec, const FLUID_VELOCITY * const __restrict__ u, const PRECISION * const __restrict__ evec, const PRECISION * const __restrict__ rhobvec, int s, int d_ncx, int d_ncy, int d_ncz, PRECISION d_dx, PRECISION d_dy, PRECISION d_dz);
 
 
 #endif /* HydroPlus_h */
--- rhic/src/HydroPlus.cpp
+++ rhic/src/HydroPlus.cpp
@@ -7,34 +7,75 @@
 
 // Equation indices from PRD 98 (2018) 036006
 
+#include <stdlib.h>
 #include <math.h>
 #include <cmath>
 
+#include "../include/InitialConditions.h"
 #include "../include/DynamicalVariables.h"
+#include "../include/LatticeParameters.h"
+#include "../include/InitialConditionParameters.h"
+#include "../include/HydroParameters.h"
+#include "../include/EquationOfState.h"
 #include "../include/HydroPlus.h"
 
-#define Cp 1.0
+#define HydroPlus
+
 #define lambdaT 1.0
 
-SLOW_MODES_Q *Qvec;
-SLOW_MODES *phiQ;
 
-//correlation length
+PRECISION *Qvec; // Q vectors of slow modes
+SLOW_MODES *PhiQ, *PhiQp, *PhiQS; // Slow modes: updated, previous, intermediate values
+SLOW_MODES *eqPhiQ, *eqPhiQp, *eqPhiQS; // Slow modes at equilibrium: updated, previous, intermediate values
 
+// allocate memory
+void allocateHostMemorySlowModes(int len) {
+    
+    size_t bytes = sizeof(PRECISION);
+
+    Qvec = (PRECISION *)calloc(NUMBER_SLOW_MODES, bytes);
+    
+    PhiQ  = (SLOW_MODES *)calloc(1, sizeof(SLOW_MODES));
+    PhiQp = (SLOW_MODES *)calloc(1, sizeof(SLOW_MODES));
+    PhiQS = (SLOW_MODES *)calloc(1, sizeof(SLOW_MODES));
+    
+    eqPhiQ  = (SLOW_MODES *)calloc(1, sizeof(SLOW_MODES));
+    eqPhiQp = (SLOW_MODES *)calloc(1, sizeof(SLOW_MODES));
+    eqPhiQS = (SLOW_MODES *)calloc(1, sizeof(SLOW_MODES));
+    
+    for(unsigned int n = 0; n < NUMBER_SLOW_MODES; ++n){
+        
+        PhiQ->phiQ[n]  = (PRECISION *)calloc(len, bytes);
+        PhiQp->phiQ[n] = (PRECISION *)calloc(len, bytes);
+        PhiQS->phiQ[n] = (PRECISION *)calloc(len, bytes);
+        
+        eqPhiQ->phiQ[n]  = (PRECISION *)calloc(len, bytes);
+        eqPhiQp->phiQ[n] = (PRECISION *)calloc(len, bytes);
+        eqPhiQS->phiQ[n] = (PRECISION *)calloc(len, bytes);
+    }
+}
+
+
+PRECISION Cp(PRECISION e, PRECISION rhob){
+    return 1.0; // to be fixed
+}
+
+// correlation length
 PRECISION xi(PRECISION e, PRECISION rhob){
     return 1.0; // correlation length, to be fixed
 }
 
-//fluctuations at equilibrium
-
+// universal function
 PRECISION f2(PRECISION x){
     return 1.0 / (1.0 + x*x); // Eq. (93)
 }
 
+// slow modes with Q=0
 PRECISION equilibriumPhi0(PRECISION e, PRECISION rhob){
-    return Cp / (rhob * rhob); // Cp needs to be fixed, Eq.(90)
+    return Cp(e, rhob) / (rhob * rhob); // Cp needs to be fixed, Eq.(90)
 }
 
+// slow modes with Q
 PRECISION equilibriumPhiQ(PRECISION e, PRECISION rhob, PRECISION Q){
     PRECISION corrL = xi(e, rhob);
     PRECISION qL = Q * corrL;
@@ -42,7 +83,6 @@ PRECISION equilibriumPhiQ(PRECISION e, PRECISION rhob, PRECISION Q){
 }
 
 //relaxation of fluctuations
-
 PRECISION relaxationCoefficientPhiQ(PRECISION e, PRECISION rhob, PRECISION Q){
     PRECISION corrL = xi(e, rhob);
     PRECISION qL = Q * corrL;
@@ -50,26 +90,79 @@ PRECISION relaxationCoefficientPhiQ(PRECISION e, PRECISION rhob, PRECISION Q){
     PRECISION qL3 = qL2 * qL;
     PRECISION qL4 = qL3 * qL;
     
-    return lambdaT/(Cp * corrL * corrL) * (qL2 + qL4);
+    return lambdaT/(Cp(e, rhob) * corrL * corrL) * (qL2 + qL4);
 }
 
-//source terms of extra modes, Eq. (76)
+void setInitialConditionSlowModes(void * latticeParams, void * hydroParams)
+{
+    struct LatticeParameters * lattice = (struct LatticeParameters *) latticeParams;
+    struct HydroParameters * hydro = (struct HydroParameters *) hydroParams;
+    int nx = lattice->numLatticePointsX;
+    int ny = lattice->numLatticePointsY;
+    int nz = lattice->numLatticePointsRapidity;
+    
+    Qvec[0] = 0.1;
+    Qvec[1] = 1.0;
+    Qvec[2] = 2.0;
+    
+    for(unsigned int n = 0; n < NUMBER_SLOW_MODES; ++n){
+        
+        PRECISION Qn = Qvec[n];
+        
+        for(int i = 2; i < nx+2; ++i) {
+            for(int j = 2; j < ny+2; ++j) {
+                for(int k = 2; k < nz+2; ++k) {
+                    
+                    int s = columnMajorLinearIndex(i, j, k, nx+4, ny+4);
+                    
+                    PRECISION es = e[s];
+                    PRECISION rhobs = rhob[s];
+                
+                    eqPhiQ->phiQ[n][s] = equilibriumPhiQ(es, rhobs, Qn);
+                    eqPhiQp->phiQ[n][s] = eqPhiQ->phiQ[n][s];
+                
+                    PhiQ->phiQ[n][s] = eqPhiQ->phiQ[n][s];
+                    PhiQp->phiQ[n][s] = eqPhiQ->phiQ[n][s];
+                }
+            }
+        }
+    }
+}
 
-void setSlowModesSourceTerms(PRECISION * const __restrict__ PhiQRHS, const PRECISION * const __restrict__ equilibriumPhiQ, const PRECISION * const __restrict__ PhiQ, const SLOW_MODES_Q * const __restrict__ Qvec, PRECISION e, PRECISION rhob, PRECISION ut, PRECISION dkvk){
+
+//contributions from slow modes to primary variables
+PRECISION entropySlowModes(const PRECISION * const __restrict__ equilibriumPhiQ, const PRECISION * const __restrict__ PhiQ, const PRECISION * const __restrict__ Q){
+    // entropy of slow modes on a single cell
+    
+    PRECISION entropy = 0.0;
+    
+    for(unsigned int n = 0; n < NUMBER_SLOW_MODES; ++n){
+        PRECISION deltaQ = Q[n+1] - Q[n]; // to be fixed
+        PRECISION phiRatio = PhiQ[n] / equilibriumPhiQ[n];
+        PRECISION integrand = Q[n]*Q[n] * (log(phiRatio) - phiRatio + 1);
+        entropy += deltaQ * integrand;
+    }
+    
+    return entropy;
+}
+
+
+//source terms of extra modes, Eq. (76)
+void setSlowModesSourceTerms(PRECISION * const __restrict__ PhiQRHS, const PRECISION * const __restrict__ equilibriumPhiQ, const PRECISION * const __restrict__ PhiQ, const PRECISION * const __restrict__ Qvec, PRECISION e, PRECISION rhob, PRECISION ut, PRECISION dkvk){
     
     PRECISION gammaQ[NUMBER_SLOW_MODES];
-    PRECISION Q[NUMBER_SLOW_MODES];
     PRECISION utInv = 1.0/ut;
     
     for(unsigned int n = 0; n < NUMBER_SLOW_MODES; ++n){
-        *Q[n] = Qvec->qvec[n];
-        gammaQ[n] = relaxationCoefficientPhiQ(e, rhob, Q[n]);
+        PRECISION Qn = Qvec[n];
+        gammaQ[n] = relaxationCoefficientPhiQ(e, rhob, Qn);
         PhiQRHS[n] = utInv * (-2) * gammaQ[n] * (PhiQ[n] - equilibriumPhiQ[n]) + PhiQ[n] * dkvk;
     }
     
 }
 
-void loadSlowModesSourceTerms(const SLOW_MODES * const __restrict__ equilibriumSlowModes, const SLOW_MODES * const __restrict__ SlowModes, PRECISION * const __restrict__ S, const SLOW_MODES_Q * const __restrict__ Qvec, const FLUID_VELOCITY * const __restrict__ u, const PRECISION * const __restrict__ evec, const PRECISION * const __restrict__ rhobvec, int s, int d_ncx, int d_ncy, int d_ncz, PRECISION d_dx, PRECISION d_dy, PRECISION d_dz){ // Modes, slow modes; S, source terms;
+void loadSlowModesSourceTerms(const SLOW_MODES * const __restrict__ equilibriumSlowModes, const SLOW_MODES * const __restrict__ SlowModes, PRECISION * const __restrict__ S, const PRECISION * const __restrict__ Qvec, const FLUID_VELOCITY * const __restrict__ u, const PRECISION * const __restrict__ evec, const PRECISION * const __restrict__ rhobvec, int s, int d_ncx, int d_ncy, int d_ncz, PRECISION d_dx, PRECISION d_dy, PRECISION d_dz){
+    // Modes, slow modes; S, source terms;
     
     //=========================================================
     // primary variables
@@ -95,10 +188,10 @@ void loadSlowModesSourceTerms(const SLOW_MODES * const __restrict__ equilibriumS
     
     for(unsigned int n = 0; n < NUMBER_SLOW_MODES; ++n){
         
-        *equilibriumPhiQvec[n] = equilibriumSlowModes->phiQ[n];
+        equilibriumPhiQvec[n] = equilibriumSlowModes->phiQ[n];
         equilibriumPhiQ[n] = equilibriumPhiQvec[n][s];
         
-        *PhiQvec[n] = SlowModes->phiQ[n];
+        PhiQvec[n] = SlowModes->phiQ[n];
         PhiQ[n] = PhiQvec[n][s];
         
     }
